def766007807a89ef3157b2cbcb01a62fb77522c
FilipeS4ntts <filipesantanawrk@gmail.com>
2025-06-24 09:50:34 -0300

    change

---
+++ filipe/modalSpotify-Swift-/modal/modal/LibraryScreen.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class LibraryScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBOutlet weak var tvlibrary: UITableView!
+    
+
+}
